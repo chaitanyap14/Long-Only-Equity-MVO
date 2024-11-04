4b6ac44fe90a53ea6eae2ea02c39c5b7bf9aa098
chaitanyap14 <cpalghadmal14@gmail.com>
2024-11-03 19:47:44 -0500

    Removed SPX constituents csv

--- mvo.py
+++ mvo.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 from datetime import datetime
 
-#spx = pd.read_csv("./SPX_500_Historical_Constituents(08-17-2024).csv")
-stocks = ['XLY', 'XLP', 'XLE', 'XLF', 'XLV', 'XLI', 'XLB', 'XLK', 'XLU']
+stocks = ['AAPL', 'NVDA', 'MSFT', 'AMZN', 'META', 'GOOGL', 'BRK-B', 'AVGO', 'GOOG', 'TSLA', 'LLY', 'JPM', 'XOM', 'UNH', 'V']
+#stocks = ['XLY', 'XLP', 'XLE', 'XLF', 'XLV', 'XLI', 'XLB', 'XLK', 'XLU']
 
-dataset = yf.download(stocks, start=datetime(2013, 10, 30), end=datetime(2024, 10, 31))['Adj Close']
+dataset = yf.download(stocks, start=datetime(2019, 10, 30), end=datetime(2024, 10, 31))['Adj Close']
 
 def log_return(data):
     log_return = np.log(data/data.shift(1))
     return log_return[1:]
 
 log_daily_return = log_return(dataset)
-print(log_daily_return)
+print(log_daily_return.mean())
 
 dataset.plot(figsize=(10,6))
 plt.show()
@@ -28,6 +28,8 @@ def portfolio_gen(returns):
     portfolio_weights = []
 
     for i in range(NUM_PORTFOLIOS):
+        #w = uniform(-0.3, 0.7, len(stocks))
+        #w /= np.sum(np.abs(w))
         w = random(len(stocks))
         w /= np.sum(w)
         portfolio_weights.append(w)
@@ -47,17 +49,21 @@ def optimal_portfolio(sr, r, v, w):
 
 lookback = 252
 rebalance_freq = 126
-NUM_PORTFOLIOS = 1000
-actual_returns = 0
-expected_sr = 0
+NUM_PORTFOLIOS = 10000
+actual_returns = []
+expected_sr = []
 
 for i in range(lookback, len(log_daily_return), rebalance_freq):
     returns, risk, weights = portfolio_gen(log_daily_return[i:i-lookback:-1])
     r, v, sr, w = optimal_portfolio(returns/risk, returns, risk, weights)
-    actual_returns += np.sum(log_daily_return[i:i+rebalance_freq].mean()*w[0])*rebalance_freq
-    expected_sr += sr
+    actual_returns.append(np.sum(log_daily_return[i:i+rebalance_freq-1].mean()*w[0])*rebalance_freq)
+    expected_sr.append(sr)
 
-print("Annualized Log Returns: ", actual_returns/4)
-print("Annualized Returns: ", np.exp(actual_returns/4)-1)
-print("Average Expected Sharpe Ratio: ", expected_sr/8)
+annualized_logreturn = np.mean(actual_returns)*(lookback/rebalance_freq)
+print("Annualized log return: ", annualized_logreturn)
+print("Annualized actual return: ", np.exp(annualized_logreturn)-1)
+print("Expected Sharpe Ratio: ", np.mean(expected_sr))
+#print("Annualized Log Returns: ", actual_returns/2)
+#print("Annualized Returns: ", np.exp(actual_returns/2)-1)
+#print("Average Expected Sharpe Ratio: ", expected_sr/4)
 
